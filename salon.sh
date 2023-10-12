86c3930bcb882608e116626ed0a497bde5938e3e
S. Guerra <113545692+LeStelio@users.noreply.github.com>
2023-10-12 01:45:12 -0500

    Submission commit

---
+++ salon.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+PSQL="psql -X --username=freecodecamp --dbname=salon --tuples-only -c"
+
+echo -e "\n~~~~~ MY SALON ~~~~~"
+
+MENU() {
+  if [[ $1 ]]
+  then
+    echo -e "\n$1"
+  else
+    echo -e "\nWelcome to My Salon, how can I help you?"
+  fi
+
+  # display services
+  SERVICES=$($PSQL "SELECT service_id, name FROM services ORDER BY service_id")
+  echo "$SERVICES" | while read SERVICE_ID BAR NAME
+  do
+    echo "$SERVICE_ID) $NAME"
+  done
+
+  read SERVICE_ID_SELECTED
+
+  # if input is not a number
+  if [[ ! $SERVICE_ID_SELECTED =~ ^[1-5]$ ]]
+  then
+    # send to menu
+    MENU "I could not find that service. What would you like today?"
+  else  
+    # ask for phone number
+    echo -e "\nWhat's your phone number?"
+    read CUSTOMER_PHONE
+
+    CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone = '$CUSTOMER_PHONE'")
+
+    # if customer doesn't exist
+    if [[ -z $CUSTOMER_NAME ]]
+    then
+      echo -e "\nI don't have a record for that phone number, what's your name?"
+      # get new customer's name
+      read CUSTOMER_NAME
+      # insert new customer
+      INSERT_CUSTOMER_RESULT=$($PSQL "INSERT INTO customers(name, phone) VALUES('$CUSTOMER_NAME', '$CUSTOMER_PHONE')")
+    fi
+    # get customer_id
+    CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone = '$CUSTOMER_PHONE'")
+
+    # get service name
+    SERVICE_NAME=$($PSQL "SELECT name FROM services WHERE service_id = $SERVICE_ID_SELECTED")
+
+    # get appointment time
+    echo -e "\nWhat time would you like your $(echo $SERVICE_NAME | sed -r 's/^ *| *$//g'), $(echo $CUSTOMER_NAME | sed -r 's/^ *| *$//g')?"
+    
+    read SERVICE_TIME
+
+    # insert appointment
+    APPOINTMENT_RESULT=$($PSQL "INSERT INTO appointments(customer_id, service_id, time) VALUES($CUSTOMER_ID, $SERVICE_ID_SELECTED, '$SERVICE_TIME')")
+    if [[ -z $APPOINTMENT_RESULT ]]
+    then
+      echo "appointment failed. Please retry."
+    else
+      echo -e "\nI have put you down for a $(echo $SERVICE_NAME | sed -r 's/^ *| *$//g') at $SERVICE_TIME, $(echo $CUSTOMER_NAME | sed -r 's/^ *| *$//g')."
+    fi
+  fi
+}
+MENU
